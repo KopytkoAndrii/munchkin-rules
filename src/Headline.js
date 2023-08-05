ca43161deea73957db4f24743e453037cadb0fd5
YOvJIK <mrnetilo14@gmail.com>
2023-08-05 19:10:17 +0300

    Fonts fix

--- src/Headline.js
+++ src/Headline.js
@@ -4,7 +4,7 @@ export const Headline = styled.h1`
   font-size: 2rem;
   text-align: center;
   color: hsla(4, 55%, 15%, 1);
-  font-family: "Overlock", cursive;
+  font-family: "Lora", cursive;
   text-transform: uppercase;
   text-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
   font-weight: 700;
